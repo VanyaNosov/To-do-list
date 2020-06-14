bdd0682c0db4c6577dee665c8390a99121240202
vanyanosov <nosov582125@gmail.com>
2020-06-14 21:36:33 +0300

    structure

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import Header from '../packages/auth/container/sign_in/registration.jsx';
+
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[folder]__[local]--[hash:base64:5]',
+              },
+            },
+          },
+        ],
+      }
+    ]
+  },
+} 
